ce25dd2b351ffdae3b3ae2dbc393efe15b3d1715
pfaion <p.faion@web.de>
2015-07-11 19:11:33 +0200

    displaying data works now!

--- timelinewidget.cpp
+++ timelinewidget.cpp
@@ -14,10 +14,13 @@
 #include <QJsonValue>
 #include <QJsonArray>
 #include <QDir>
+#include <QLabel>
 
 TimelineWidget::TimelineWidget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::TimelineWidget)
+    ui(new Ui::TimelineWidget),
+    rowHeight(40),
+    colWidth(40)
 {
     ui->setupUi(this);
 
@@ -30,10 +33,12 @@ TimelineWidget::TimelineWidget(QWidget *parent) :
     // list alignment to top
     ui->scrollAreaWidgetContents->layout()->setAlignment(Qt::AlignTop);
 
-    // add a clean scene
+    // setup graphicsscene
     QGraphicsView* view = ui->graphicsView;
     QGraphicsScene* s = new QGraphicsScene(0);
     view->setScene(s);
+    // TODO: antialiasing??
+    view->setRenderHint(QPainter::Antialiasing);
 
     // align origins of scene and view
     // TODO: only workaround. is there a better way?
@@ -48,6 +53,7 @@ TimelineWidget::TimelineWidget(QWidget *parent) :
     connect(view->horizontalScrollBar(), SIGNAL(valueChanged(int)), ui->header->horizontalScrollBar(), SLOT(setValue(int)));
     connect(ui->header->horizontalScrollBar(), SIGNAL(valueChanged(int)), view->horizontalScrollBar(), SLOT(setValue(int)));
 
+    // setup header graphicsscene
     QGraphicsScene* hs = new QGraphicsScene(0);
     ui->header->setScene(hs);
     ui->header->setAlignment(Qt::AlignLeft | Qt::AlignTop);
@@ -55,12 +61,28 @@ TimelineWidget::TimelineWidget(QWidget *parent) :
     for(int i=0; i<15; i++) {
         QGraphicsTextItem* txt = new QGraphicsTextItem(QString::number(i+1));
         int w = txt->boundingRect().width();
-        txt->setPos(40*i+20 -w/2,0);
+        txt->setPos(colCenter(i) - w/2,0);
         hs->addItem(txt);
     }
 
     // Test Data
     QList<DataObject> objects;
+    {
+        DataObject auto1("Automobil 1");
+        auto1.data.append(DataPoint(1,1));
+        auto1.data.append(DataPoint(5,1,1));
+        auto1.data.append(DataPoint(10,1));
+        auto1.data.append(DataPoint(15,1));
+        objects.append(auto1);
+        DataObject auto2("Automobil 2");
+        auto2.data.append(DataPoint(1,1));
+        auto2.data.append(DataPoint(2,1));
+        auto2.data.append(DataPoint(3,1));
+        auto2.data.append(DataPoint(4,1));
+        auto2.data.append(DataPoint(5,1,0));
+        auto2.data.append(DataPoint(12,1));
+        objects.append(auto2);
+    }
     for(int i=0; i<5; i++) {
         DataObject auto1("Auto "+QString::number(2*i));
         auto1.data.append(DataPoint(1,1));
@@ -76,6 +98,23 @@ TimelineWidget::TimelineWidget(QWidget *parent) :
         auto2.data.append(DataPoint(5,1));
         auto2.data.append(DataPoint(12,1));
         objects.append(auto2);
+    }
+
+    // which frames are used
+    QList<int> framesToDraw;
+    foreach(DataObject obj, objects) {
+        foreach(DataPoint p, obj.data) {
+            if(!framesToDraw.contains(p.frame)) framesToDraw.append(p.frame);
+        }
+    }
+    qSort(framesToDraw);
+
+    // draw framelines
+    QPen framelinePen;
+    framelinePen.setColor(Qt::gray);
+    framelinePen.setStyle(Qt::DotLine);
+    foreach(int f, framesToDraw) {
+        s->addLine(colCenter(f-1), 0, colCenter(f-1), objects.length() * rowHeight, framelinePen);
     }
 
     // parse data
@@ -94,16 +133,38 @@ TimelineWidget::TimelineWidget(QWidget *parent) :
 
         ui->scrollAreaWidgetContents->layout()->addWidget(frame);
 
-        // add data
+        // add data lines
+        int pointradius = 5;
         for(int j=0; j<obj.data.length()-1; j++) {
+            QPen linePen;
+            linePen.setColor(Qt::red);
+            linePen.setWidth(2);
             int f1 = obj.data[j].frame-1;
             int f2 = obj.data[j+1].frame-1;
-            s->addLine(40*f1+20, 40*i+20, 40*f2+20, 40*i+20, QPen(Qt::red));
-            s->addEllipse(40*f1+15, 40*i+15, 10, 10, QPen(Qt::black));
+            // test if swap
+            if(obj.data[j+1].changeWith != -1) {
+                // test if prefill needed
+                int fstart = f1;
+                if(f2-1 > f1) {
+                    fstart = f2-1;
+                    s->addLine(colCenter(f1), rowCenter(i), colCenter(fstart), rowCenter(i), linePen);
+                }
+                int cw = obj.data[j+1].changeWith;
+                QPainterPath path;
+                path.moveTo(colCenter(fstart),rowCenter(i));
+                path.cubicTo(colCenter(f2)-colWidth/2, rowCenter(i), colCenter(fstart)+colWidth/2, rowCenter(cw), colCenter(f2), rowCenter(cw));
+
+                QGraphicsPathItem* pathitem =  s->addPath(path, linePen);
+            } else {
+                s->addLine(colCenter(f1), rowCenter(i), colCenter(f2), rowCenter(i), linePen);
+            }
         }
-        // last index
-        int f = obj.data.last().frame-1;
-        s->addEllipse(40*f+15, 40*i+15, 10, 10, QPen(Qt::black));
+        // add point handles
+        for(int j=0; j<obj.data.length(); j++) {
+            int f = obj.data[j].frame-1;
+            QGraphicsEllipseItem* handle = s->addEllipse(colCenter(f) - pointradius, rowCenter(i) - pointradius, 2*pointradius, 2*pointradius, QPen(Qt::black), QBrush(Qt::black));
+            handle->setZValue(1);
+        }
     }
 
 
--- timelinewidget.h
+++ timelinewidget.h
@@ -22,10 +22,17 @@ public:
 private:
     Ui::TimelineWidget *ui;
 
+    int rowHeight;
+    int colWidth;
+
+    inline int rowCenter(int row) { return rowHeight * row + rowHeight/2; };
+    inline int colCenter(int col) { return colWidth * col + colWidth/2; };
+
     struct DataPoint {
         int frame;
         int type;
-        DataPoint(int f, int t): frame(f), type(t) {}
+        int changeWith;
+        DataPoint(int f, int t, int cw = -1): frame(f), type(t), changeWith(cw) {}
     };
 
     struct DataObject {
